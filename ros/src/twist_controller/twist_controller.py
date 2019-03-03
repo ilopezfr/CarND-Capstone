0fc921e3eaa20e9fb8be154cc9c9b489ac456226
Andre Strobel <38402937+CyberAMS@users.noreply.github.com>
2019-03-02 16:55:50 -0800

    Changed debugging frequency

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -5,7 +5,7 @@ from lowpass import LowPassFilter
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-DEBUG_THRESHOLD = 500
+DEBUG_THRESHOLD = 50
 
 class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
